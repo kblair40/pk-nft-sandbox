491a89281bee145fd58c87fb58f50f6f1236b41d
Kevin Blair <kblair40@gmail.com>
2022-07-13 09:50:31 -0500

    Add skeletons for mock deployment and test nft deployment

---
+++ deploy/01-deploy-nft.js
@@ -0,0 +1,43 @@
+const { network, ethers } = require("hardhat");
+const { developmentChains, networkConfig } = require("../helper-hardhat-config");
+const { verify } = require("../utils/verify");
+const fs = require("fs");
+// const { storeImages, storeTokenUriMetadata } = require("../utils/uploadToPinata")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments;
+    const { deployer } = await getNamedAccounts();
+
+    // const chainId = network.config.chainId
+    // let ethUsdPriceFeedAddress
+
+    // if (developmentChains.includes(network.name)) {
+    //     const EthUsdAggregator = await ethers.getContract("MockV3Aggregator")
+    //     ethUsdPriceFeedAddress = EthUsdAggregator.address
+    // } else {
+    //     ethUsdPriceFeedAddress = networkConfig[chainId].ethUsdPriceFeed
+    // }
+
+    log("---------------------------------------");
+
+    const svg = await fs.readFileSync("./images/pklogo.svg", {
+        encoding: "utf8",
+    });
+
+    // args = [ethUsdPriceFeedAddress, lowSVG, highSVG]
+
+    const pkNft = await deploy("PkNft", {
+        from: deployer,
+        // args: args,
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    });
+
+    // Verify the deployment
+    // if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+    //     log("Verifying...")
+    //     await verify(dynamicSvgNft.address, args)
+    // }
+};
+
+// module.exports.tags = ["all", "dynamicvsg", "main"]
