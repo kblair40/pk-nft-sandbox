3f86341c0eca8065b678135986be216063f60805
Kevin Blair <kblair40@gmail.com>
2022-07-14 13:13:48 -0500

    Add comments and rename to PK SVG NFT

--- contracts/PkNft.sol
+++ contracts/PkNft.sol
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
@@ -9,16 +10,19 @@ contract PkNft is ERC721 {
     string internal s_tokenUri;
     string private constant base64EncodedSvgPrefix = "data:image/svg+xml;base64,";
 
-    constructor(string memory imageUri) ERC721("PK Logo 3", "PKC") {
+    constructor(string memory imageUri) ERC721("PK SVG NFT", "PKSN") {
         s_tokenCounter = 0;
         s_tokenUri = svgToImageURI(imageUri);
     }
 
     function svgToImageURI(string memory svg) public pure returns (string memory) {
         string memory svgBase64Encoded = Base64.encode(bytes(string(abi.encodePacked(svg))));
+
+        // concatenate base64EncodedSvgPrefix with svgBase64Encoded
         return string(abi.encodePacked(base64EncodedSvgPrefix, svgBase64Encoded));
     }
 
+    // Must override this function when minting
     function _baseURI() internal pure override returns (string memory) {
         // data:application/json;base64, is prefix for base64 json
         return "data:application/json;base64,";
@@ -33,7 +37,6 @@ contract PkNft is ERC721 {
 
     function tokenURI(uint256) public view override returns (string memory) {
         // overrides the inherited tokenURI getter to return the contract's tokenURI
-
         return
             string(
                 abi.encodePacked(
@@ -47,6 +50,7 @@ contract PkNft is ERC721 {
                                 name(),
                                 '", "description":"Testing SVG NFT Contracts", ',
                                 '"attributes": [{"trait_type": "coolness", "value": 100}], "image":"',
+                                // s_tokenUri was already prefixed with base64EncodedSvgPrefix in constructor
                                 s_tokenUri,
                                 '"}'
                             )
