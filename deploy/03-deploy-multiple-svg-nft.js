64ff7e7fca0f19745a04b391785a2cb10ccf4424
Kevin Blair <kblair40@gmail.com>
2022-07-15 07:18:10 -0500

    Add skeletons for multiple svg contract and corresponding deploy script

--- deploy/03-deploy-multiple-svg-nft.js
+++ deploy/03-deploy-multiple-svg-nft.js
@@ -3,17 +3,19 @@ const { developmentChains } = require("../helper-hardhat-config");
 const { verify } = require("../utils/verify");
 const fs = require("fs");
 
+// New version where multiple svgs are sent at the same time
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments;
     const { deployer } = await getNamedAccounts();
-
-    // for SVG images, this is best practice for reading/encoding.
-    // Other images ex. png, jpg etc. are better to pin to IPFS and then use the IPFS url
-    const svg = await fs.readFileSync("./images/pklogo.svg", {
-        encoding: "utf8",
-    });
-
-    args = [svg];
+    const SVGs = [];
+    const colors = ["blue", "green", "red", "yellow"];
+    for (let color of colors) {
+        const svg = await fs.readFileSync(`./images/pklogo${color}.svg`, {
+            encoding: "utf8",
+        });
+        SVGs.push(svg);
+    }
+    args = [SVGs];
     const pkSvgNft = await deploy("PkSvgNft", {
         from: deployer,
         args: args,
@@ -21,14 +23,13 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
         waitConfirmations: network.config.blockConfirmations || 1,
     });
 
-    console.log("---------------------------------------");
+    log("---------------------------------------");
     console.log("Deployed to...", pkSvgNft.address);
-
     // Verify the deployment
     if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
-        console.log("Verifying...");
+        log("Verifying...");
         await verify(pkSvgNft.address, args);
     }
 };
 
-module.exports.tags = ["all", "svg", "main"];
+module.exports.tags = ["all", "multiple", "main"];
