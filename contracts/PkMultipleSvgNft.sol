5fd8ecbfd891f2830c45332b41b8b453d94b35a8
Kevin Blair <kblair40@gmail.com>
2022-07-15 07:57:56 -0500

    Finish all methods except mint

---
+++ contracts/PkMultipleSvgNft.sol
@@ -0,0 +1,88 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "base64-sol/base64.sol";
+
+contract PkMultipleSvgNft is ERC721 {
+    uint256 private s_tokenCounter;
+    string[4] internal s_tokenUris;
+    // string internal s_blueTokenUri;
+    // string internal s_greenTokenUri;
+    // string internal s_redTokenUri;
+    // string internal s_yellowTokenUri;
+    string private constant base64EncodedSvgPrefix = "data:image/svg+xml;base64,";
+
+    constructor(
+        // string memory blueUri,
+        // string memory greenUri,
+        // string memory redUri,
+        // string memory yellowUri
+        string[4] memory tokenUris
+    ) ERC721("PK MULTISVG", "PKS") {
+        s_tokenCounter = 0;
+        s_tokenUris = tokenUris;
+        // s_blueTokenUri = svgToImageURI(blueUri);
+        // s_greenTokenUri = svgToImageURI(greenUri);
+        // s_redTokenUri = svgToImageURI(redUri);
+        // s_yellowTokenUri = svgToImageURI(yellowUri);
+    }
+
+    function svgToImageURI(string memory svg) public pure returns (string memory) {
+        string memory svgBase64Encoded = Base64.encode(bytes(string(abi.encodePacked(svg))));
+
+        // concatenate base64EncodedSvgPrefix with svgBase64Encoded
+        return string(abi.encodePacked(base64EncodedSvgPrefix, svgBase64Encoded));
+    }
+
+    // Must override this function when minting
+    function _baseURI() internal pure override returns (string memory) {
+        // data:application/json;base64, is prefix for base64 json
+        return "data:application/json;base64,";
+    }
+
+    function mintNft() public returns (uint256) {
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter = s_tokenCounter + 1;
+
+        return s_tokenCounter;
+    }
+
+    function tokenURI(uint256 tokenIdx) public view override returns (string memory) {
+        // overrides the inherited tokenURI getter to return the contract's tokenURI
+        string[4] memory names = ["PK Blue", "PK Green", "PK Red", "PK Yellow"];
+        string[4] memory hexes = ["87ceeb", "3ae57f", "d12f50", "ffea00"];
+
+        return
+            string(
+                abi.encodePacked(
+                    // concatenate baseURI with the Base64 encoding
+                    _baseURI(),
+                    // typecase to bytes so the json can be Base64 encoded
+                    Base64.encode(
+                        bytes(
+                            abi.encodePacked(
+                                '{"name": "',
+                                names[tokenIdx],
+                                // 'PK Red'
+                                // name(),
+                                '", "description":"Testing SVG NFT Contracts", ',
+                                // '"attributes": [{"trait_type": "bgColorHex", "value": "d12f50"}], "image":"',
+                                '"attributes": [{"trait_type": "bgColorHex", "value": "',
+                                hexes[tokenIdx],
+                                ' "}], "image":"',
+                                // s_tokenUri was already prefixed with base64EncodedSvgPrefix in constructor
+                                s_tokenUris[tokenIdx],
+                                '"}'
+                            )
+                        )
+                    )
+                )
+            );
+    }
+
+    function getTokenCounter() public view returns (uint256) {
+        return s_tokenCounter;
+    }
+}
--- deploy/03-deploy-multiple-svg-nft.js
+++ deploy/03-deploy-multiple-svg-nft.js
@@ -9,14 +9,18 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deployer } = await getNamedAccounts();
     const SVGs = [];
     const colors = ["blue", "green", "red", "yellow"];
+
     for (let color of colors) {
         const svg = await fs.readFileSync(`./images/pklogo${color}.svg`, {
             encoding: "utf8",
         });
         SVGs.push(svg);
     }
+
+    // console.log("ALL SVGS:", SVGs);
+
     args = [SVGs];
-    const pkSvgNft = await deploy("PkSvgNft", {
+    const pkMultipleSvgNft = await deploy("PkMultipleSvgNft", {
         from: deployer,
         args: args,
         log: true,
@@ -24,12 +28,19 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     });
 
     log("---------------------------------------");
-    console.log("Deployed to...", pkSvgNft.address);
+    console.log("Deployed to...", pkMultipleSvgNft.address);
     // Verify the deployment
     if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
         log("Verifying...");
-        await verify(pkSvgNft.address, args);
+        await verify(pkMultipleSvgNft.address, args);
     }
 };
 
-module.exports.tags = ["all", "multiple", "main"];
+module.exports.tags = ["all", "multi", "main"];
+
+// const tokenUris = [
+//     "ipfs://QmV8rQ6hftaAsTRTNWWXWXh7rU9jSnBfuxWwDJEV9MCmBf", // blue
+//     "ipfs://QmbLGbc78mBFacjePNUsVDGDYGvBaArvgKddk3SVXwnniY", // green
+//     "ipfs://QmeMrWk8kxU7dbdqyKJziLhzJhgm9GWLjnShTr7Tzo1g77", // red
+//     "ipfs://Qmc8XEuXp8L3Q69KPnztvyQSYuVp6GXsKzDZWBNS565RtA", // yellow
+// ];
