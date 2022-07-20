169241fb2e80849808257b260c48e1c2657e3464
Kevin Blair <kblair40@gmail.com>
2022-07-20 06:24:19 -0500

    remove unused imports/variables

--- scripts/mint-nft.js
+++ scripts/mint-nft.js
@@ -1,37 +1,18 @@
-const { ethers, network } = require("hardhat");
-const { developmentChains } = require("../helper-hardhat-config.js");
-const { verify } = require("../utils/verify");
-// const { moveBlocks } = require("../utils/move-blocks");
+const { ethers } = require("hardhat");
 
 async function mintNft() {
-    // const pkSvgNft = await ethers.getContract("PkSvgNft");
     const basicSvgNft = await ethers.getContract("BasicSvgNft");
     console.log("\nMinting...");
 
     // mint a basicSvgNft
     const mintTx = await basicSvgNft.mintNft();
     const mintTxReceipt = await mintTx.wait(1);
-    console.log("Minted!\n");
+    console.log("Minted!");
+    console.log("RECEIPT:", mintTxReceipt);
 
-    // this works because we emit the tokenId in the DogMinted event in the contract
+    // this works because the tokenId is emitted in the PkSvgMinted event in the contract
     const tokenId = mintTxReceipt.events[0].args.tokenId;
-    console.log("ID OF MINTED TOKEN:", tokenId);
-
-    // console.log("Approving Nft...");
-
-    // const approvalTx = await basicSvgNft.approve(nftMarketplace.address, tokenId);
-    // await approvalTx.wait(1);
-    // console.log("Listing NFT...");
-    // const tx = await nftMarketplace.listItem(basicNft.address, tokenId, PRICE);
-    // await tx.wait(1);
-    // console.log("Listed!");
-
-    if (network.config.chainId == "31337") {
-        // Moralis has a hard time if you move more than 1 at once!
-        // Mining 1 block gives moralis time to index our event
-        // await moveBlocks(2, (sleepAmount = 1000));
-        console.log('chainId == "31337"');
-    }
+    console.log("ID OF MINTED TOKEN:", tokenId.toString());
 }
 
 // When this is run, Moralis should notice a new nft was minted and listed (ListItem event) and update the database
